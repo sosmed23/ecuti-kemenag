9b3a62db6853f7ef37e21df090057f72badc9250
sosmed23 <aapksosmed23@gmail.com>
2025-07-05 23:23:28 +0800

    Versi stabil siap untuk di-online-kan

--- kemenag_pohuwato_cuti/settings.py
+++ kemenag_pohuwato_cuti/settings.py
@@ -85,6 +85,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT tidak diperlukan untuk development lokal
 
 # Media files (Uploads)
